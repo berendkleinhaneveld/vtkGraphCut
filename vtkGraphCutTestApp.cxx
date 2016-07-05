a0223215bd73162d4cb4abba68d7192950ae414c
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2016-07-05 22:03:14 +0200

    Move tests for edge to its own test file

--- vtkGraphCutTestApp.cxx
+++ vtkGraphCutTestApp.cxx
@@ -70,57 +70,6 @@ vtkImageData* createTestImageData() {
 }
 
 /**
- * Tests the methods of an vtkEdge struct.
- * - isTerminal
- * - flowFromNode
- * - capacityFromNode
- * - setCapacity
- * - isSaturatedFromNode
- * - addFlowFromNode
- */
-void testEdgeStruct() {
-    std::cout << __FUNCTION__ << "\n";
-    vtkEdge edge = vtkEdge(SINK, 1);
-    assert(edge.isTerminal());
-    assert(edge.flowFromNode(0) == 0);
-    assert(edge.capacityFromNode(0) == 0);
-    
-    edge = vtkEdge(0, 1);
-    assert(!edge.isTerminal());
-    
-    edge.setCapacity(5);
-    assert(edge.flowFromNode(0) == 0);
-    assert(!edge.isSaturatedFromNode(0));
-    assert(!edge.isSaturatedFromNode(0));
-    assert(edge.capacityFromNode(0) == 5);
-    assert(edge.capacityFromNode(1) == 5);
-    
-    edge.addFlowFromNode(0, 5);
-    assert(edge.flowFromNode(0) == 5);
-    assert(edge.flowFromNode(1) == -5);
-    assert(edge.isSaturatedFromNode(0));
-    assert(!edge.isSaturatedFromNode(1));
-    assert(edge.capacityFromNode(0) == 0);
-    assert(edge.capacityFromNode(1) == 10);
-    
-    edge.addFlowFromNode(1, 2);
-    assert(edge.flowFromNode(1) == -3);
-    assert(edge.flowFromNode(0) == 3);
-    assert(!edge.isSaturatedFromNode(0));
-    assert(!edge.isSaturatedFromNode(1));
-    assert(edge.capacityFromNode(0) == 2);
-    assert(edge.capacityFromNode(1) == 8);
-    
-    edge.addFlowFromNode(1, 8);
-    assert(!edge.isSaturatedFromNode(0));
-    assert(edge.isSaturatedFromNode(1));
-    assert(edge.capacityFromNode(0) == 10);
-    assert(edge.capacityFromNode(1) == 0);
-
-    std::cout << "Done!" << "\n";
-}
-
-/**
  * Tests the creation of nodes for different dimensions.
  * - CreateNodesForDimensions
  */
